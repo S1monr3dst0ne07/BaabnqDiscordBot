07410e85efa3f22953e224f451af6b3000cea4d8
S1mon <jaforsi.kozak@gmail.com>
2022-08-30 17:37:07 +0200

    running

--- main.py
+++ main.py
@@ -1,32 +1,88 @@
 import discord
 import sys
-
-
-
-
+import Compiler
+import threading
+import vm
+import io
 import discord, io, sys
 
         
-        
+DELIM = '```'
         
 class cServer(discord.Client):
+    def __init__(self):
+        super().__init__()
+    
+        Compiler.cUtils.Error = cServer.CompError
+        self.xCompiler = Compiler.cCompiler()
+        
+    @staticmethod
+    def CompError(xMsg):
+        raise Exception(xMsg)
+    
+    
+    def Cons(self, xTerms):
+        xBuffer = []
+        while len(xTerms) > 0 and xTerms[0] != DELIM: 
+            xBuffer.append(xTerms.pop(0))
+            
+        return xBuffer
+    
+    def Depo(self, xTerms):
+        if len(xTerms) > 0 and xTerms[0] == DELIM:
+            xTerms.pop(0)       
+    
     async def on_ready(self):
         print("Server running")
         
     async def on_message(self, xMsg):
-        if xMsg.author == client.user:
+        if xMsg.author == xServer.user:
            return
-
-        if len(xTerminals) == 0 or xTerminals[0] != "/bq":
+       
+        xTerminals = xMsg.content.split("\n")
+        if len(xTerminals) == 0 or xTerminals[0].strip() != "/bq":
+            print(f'Message "{xTerminals}" not matching')
             return
         
-        xTerminals = xMsg.content.split(" ")
+        #consume terminals
         xArgs = [x for x in xTerminals[1:] if x.strip()]
-
-        if xArgs
         
-                        
+        xCall        = self.Cons(xArgs)
+        self.Depo(xArgs)
+        xCodeBuffer  = self.Cons(xArgs)
+        self.Depo(xArgs)
+        xInputBuffer = self.Cons(xArgs)
+        self.Depo(xArgs)
+
+        try:
+            xAsm = self.xCompiler.Compile("\n".join(xCodeBuffer))
+
+        except Exception as E:
+            await xMsg.channel.send(f'<@{xMsg.author.id}> {E}')
+
+        else:
+            xVM = vm.cMain()
+            xVM.LoadFile(xAsm[0])
             
+            xTempStd = sys.stdout
+            sys.stdout = xStdOutCap = io.StringIO()
+            
+            xRunner = threading.Thread(target = xVM.Interpret)
+            xRunner.start()
+            xRunner.join(timeout = 30)
+                        
+            if xRunner.is_alive():
+                await xMsg.channel.send(f'<@{xMsg.author.id}> Timeout reached, killing runner (sorry V.V)')
+                xVM.xProgrammIndex == len(self.xLineStructures) + 100
+                
+            else:
+                xOutput = xStdOutCap.getvalue()
+                await xMsg.channel.send(f'```\n{xOutput}\n```')
+                
+            sys.stdout = xTempStd
+                
+            
+                    
 xToken = open("token.txt", "r").read()
 xServer = cServer()    
 xServer.run(xToken)
